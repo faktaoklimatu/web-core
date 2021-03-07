1a825464bdd2e8b89d038901ff48f1cd1491d456
Jan Krcal <jkrcal@google.com>
2021-03-07 20:04:02 +0100

    Refactor search js into a class

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,238 +1,245 @@
-$(document).ready(function() {
-    $('#searchbox').on('keydown', navigate);
-    $('#searchbox').on('keyup', delayedSearch);
-    $('#searchbox').on('focus', search);
-    $('#searchbox').on('blur', maybeHideSearch);
-    $('#omnisearch-suggestions').on('blur', maybeHideSearch);
-});
+class Search {
+    constructor() {
+        this.fuse = null;
+        this.searchString = null;
+        this.timeout = null;
 
-var posts = []; // will hold the json array from your site.json file
-var fuse = null;
-var searchString = null;
-var timeout = null;
+        $('#searchShow').on('click', $.proxy(this.showSearch, this));
+        $('#searchHide').on('click', $.proxy(this.hideSearch, this));
 
-function showSearch() {
-    $('nav').addClass('navbar-search');
-    $('#searchbox').focus();
-}
+        $('#searchbox').on('keydown', $.proxy(this.navigate, this));
+        $('#searchbox').on('focus', $.proxy(this.instantSearch, this));
+        $('#searchbox').on('keyup', $.proxy(this.delayedSearch, this));
+        $('#searchbox').on('blur', $.proxy(this.maybeHideSearch, this));
 
-function hideSearch() {
-    $('nav').removeClass('navbar-search');
-    hideSuggestions();
-}
-
-function maybeHideSearch(event) {
-    if ($("#searchbox")[0].contains(event.relatedTarget) ||
-        $('#omnisearch-suggestions')[0].contains(event.relatedTarget)) {
-        return;
+        $('#omnisearch-suggestions').on('blur', $.proxy(this.maybeHideSearch, this));
     }
-    hideSearch();
-}
 
-function showSuggestions() {
-    var $body = $(document.body);
-    var oldWidth = $body.innerWidth();
-    $body.width(oldWidth);
-    $('nav').width(oldWidth);
-    $body.css("overflow", "hidden");
-    $('#omnisearch-suggestions').show();
-    $('#searchbox').addClass('searchbox-open');
-}
-
-function hideSuggestions() {
-    $('#omnisearch-suggestions').hide();
-    var $body = $(document.body);
-    $body.css("overflow", "auto");
-    $body.width("auto");
-    $('nav').width("auto");
-    $('#searchbox').removeClass('searchbox-open');
-}
-
-function navigate(e) {
-    if (e.keyCode == '27') {  // escape
-        e.preventDefault();
-        hideSearch();
-        return;
+    /**
+     * Event callbacks.
+     */
+    showSearch() {
+        $('nav').addClass('navbar-search');
+        $('#searchbox').focus();
     }
-    if (e.keyCode == '13') {  // enter
-        $('#omnisearch-suggestions a.active')[0].click();
-        return false;
+
+    hideSearch() {
+        $('nav').removeClass('navbar-search');
+        this._hideSuggestions();
     }
-    if (e.keyCode == '38' || e.keyCode == '40') {  // up/down arrow
-        e.preventDefault();
-        let up = e.keyCode == '38';
-        let allLinks = $('#omnisearch-suggestions a');
-        for (let i = 0; i < allLinks.length; i++) {
-            if (allLinks.eq(i).hasClass('active')) {
-                let next = i + (up ? -1 : 1);
-                if (next >= 0 && next < allLinks.length) {
-                    allLinks.eq(i).removeClass('active');
-                    allLinks.eq(next).addClass('active');
-                    allLinks.get(next).scrollIntoView({block: "nearest", inline: "nearest"});
-                }
-                return;
-            }
+
+    maybeHideSearch(event) {
+        if ($("#searchbox")[0].contains(event.relatedTarget) ||
+            $('#omnisearch-suggestions')[0].contains(event.relatedTarget)) {
+            return;
         }
-        return;
+        this.hideSearch();
     }
-}
 
-function search(e) {
-    if (e.keyCode == '13' || e.keyCode == '27' || e.keyCode == '38' || e.keyCode == '40') {
-        return;
+    navigate(e) {
+        if (e.keyCode == '27') {  // escape
+            e.preventDefault();
+            this.hideSearch();
+            return;
+        }
+        if (e.keyCode == '13') {  // enter
+            $('#omnisearch-suggestions a.active')[0].click();
+            return false;
+        }
+        if (e.keyCode == '38' || e.keyCode == '40') {  // up/down arrow
+            e.preventDefault();
+            let up = e.keyCode == '38';
+            let allLinks = $('#omnisearch-suggestions a');
+            for (let i = 0; i < allLinks.length; i++) {
+                if (allLinks.eq(i).hasClass('active')) {
+                    let next = i + (up ? -1 : 1);
+                    if (next >= 0 && next < allLinks.length) {
+                        allLinks.eq(i).removeClass('active');
+                        allLinks.eq(next).addClass('active');
+                        allLinks.get(next).scrollIntoView({block: "nearest", inline: "nearest"});
+                    }
+                    return;
+                }
+            }
+            return;
+        }
     }
-    searchInternal();
-}
 
-function delayedSearch(e) {
-    if (e.keyCode == '13' || e.keyCode == '27' || e.keyCode == '38' || e.keyCode == '40') {
-        return;
+    instantSearch(e) {
+        if (e.keyCode == '13' || e.keyCode == '27' || e.keyCode == '38' || e.keyCode == '40') {
+            return;
+        }
+        this._initAndSearch();
     }
-    window.clearTimeout(timeout);
-    timeout = window.setTimeout(searchInternal, 200);
-}
 
-function searchInternal() {
-    fetchSiteJson(function () {
+    delayedSearch(e) {
+        if (e.keyCode == '13' || e.keyCode == '27' || e.keyCode == '38' || e.keyCode == '40') {
+            return;
+        }
+        window.clearTimeout(this.timeout);
+        this.timeout = window.setTimeout($.proxy(this._initAndSearch, this), 200);
+    }
+
+    /**
+     * Private implementation functions.
+     */
+
+    _initAndSearch() {
+        if (this.fuse === null) {
+            // Init by fetching site.json file
+            jQuery.get("/search.json", $.proxy(this._onSiteJsonFetched, this)).fail(() => {
+                // TODO: handle failure gracefully.
+            });
+            return;
+        }
+
+        this._search();
+    }
+
+    _onSiteJsonFetched (data) {
+        var options = { 		// initialize options for fuse.js
+            shouldSort: true,
+            threshold: 0.3,
+            ignoreLocation: true,
+            maxPatternLength: 32,
+            minMatchCharLength: 2,
+            includeMatches: true,
+            includeScore: true,
+            keys: [
+                {
+                    name: "title",
+                    weight: 0.2		// give title more importance
+                },
+                {
+                    name: "perex",
+                    weight: 0.3		// give perex more importance
+                },
+                {
+                    name: "content",
+                    weight: 0.6
+                }
+            ]
+        };
+        this.fuse = new Fuse(data, options);
+        this._search();
+    }
+
+    _search() {
         let searchResults = [];
         let newSearchString = $('#searchbox').val();
-        if (newSearchString != searchString) {
-            searchString = newSearchString;
-            searchResults = searchString.length > 1 ? fuse.search(searchString) : null;
-            updateResults(searchResults);
+        if (newSearchString != this.searchString) {
+            this.searchString = newSearchString;
+            searchResults = this.searchString.length > 1 ? this.fuse.search(this.searchString) : null;
+            this._updateResults(searchResults);
         }
         if ($('#omnisearch-suggestions *').length > 0) {
-            showSuggestions();
+            this._showSuggestions();
         } else {
-            hideSuggestions();
-        }
-    });
-}
-
-function getInstances(matches, shouldMatchTitle) {
-    let instances = [];
-    if (!matches)
-        return instances;
-    for (const match of matches) {
-        let isTitle = (match.key == 'title');
-        if (isTitle != shouldMatchTitle) {
-            continue;
-        }
-        for (const index of match.indices) {
-            // Ignore matches of length 1;
-            if (index[1] - index[0] == 0) continue;
-            instances.push({start: index[0], end: index[1] + 1, text: match.value});
+            this._hideSuggestions();
         }
     }
-    // Sort by match length, descending.
-    instances.sort((a,b) => (b.end - b.start) - (a.end - a.start));
-    return instances;
-}
 
-function getSnippet(matches) {
-    let instances = getInstances(matches, false);
-    if (instances.length == 0)
-        return '';
+    _showSuggestions() {
+        let $body = $(document.body);
+        let oldWidth = $body.innerWidth();
+        $body.width(oldWidth);
+        $('nav').width(oldWidth);
+        $body.css("overflow", "hidden");
+        $('#omnisearch-suggestions').show();
+        $('#searchbox').addClass('searchbox-open');
+    }
+
+    _hideSuggestions() {
+        $('#omnisearch-suggestions').hide();
+        let $body = $(document.body);
+        $body.css("overflow", "auto");
+        $body.width("auto");
+        $('nav').width("auto");
+        $('#searchbox').removeClass('searchbox-open');
+    }
+
+    _updateResults(results) {
+        $('#omnisearch-suggestions').html(this._getResultsHtml(results));
+        $('#omnisearch-suggestions a:first-of-type').addClass('active');
+    }
+
+    _getResultsHtml(results) {
+        if (results == null) {
+            return '';
+        }
+        if (results.length == 0) {
+            return '<div class="dropdown-item empty">Nic nenalezeno</div>';
+        }
+        var resultsHtml = '';
+        results.forEach(res => {
+            let item = res.item;
+            let snippet = this._getSnippet(res.matches);
+            let title = this._getTitle(res.matches, item.title);
+            resultsHtml += '<a class="dropdown-item clearfix" href="' + item.url + '">';
+            resultsHtml += '<div class="title">' + title + '</div>';
+            resultsHtml += '<div class="search-preview card">' + item.block + '</div>' +
+                           '<div class="snippet">' + snippet + '</div>' +
+                           '</a>';
+        });
+        return resultsHtml;
+    }
+
+    _getSnippet(matches) {
+        let instances = this._getKeywordInstances(matches, false);
+        if (instances.length == 0)
+            return '';
+
+        let maxSnippets = 2;
+        let context = 30;
     
-    let maxSnippets = 2;
-    let context = 30;
+        let results = [];
+        for (let i = 0; i < instances.length && i < maxSnippets; i++) {
+            let instance = instances[i];
+            let prefix = instance.text.substring(instance.start - context, instance.start);
+            let suffix = instance.text.substring(instance.end, instance.end + context);
+            let result = '';
+            if (instance.start - context - 1 >= 0) result += '...';
+            if (prefix) result += prefix;
+            result += '<span class="keyword">' + instance.text.substring(instance.start, instance.end) + '</span>';
+            if (suffix) result += suffix;
+            if (instance.end + context + 1 < instance.text.length) result += '...';
+            results.push(result);
+        }
+        return results.join(' ');
+    }
 
-    let results = [];
-    for (let i = 0; i < instances.length && i < maxSnippets; i++) {
-        let instance = instances[i];
-        let prefix = instance.text.substring(instance.start - context, instance.start);
-        let suffix = instance.text.substring(instance.end, instance.end + context);
+    _getTitle(matches, unmatchedTitle) {
+        let instances = this._getKeywordInstances(matches, true);
+        if (instances.length == 0)
+            return unmatchedTitle;
+        // Only consider the first.
+        let instance = instances[0];
+        let prefix = instance.text.substring(0, instance.start);
+        let suffix = instance.text.substring(instance.end);
         let result = '';
-        if (instance.start - context - 1 >= 0) result += '...';
         if (prefix) result += prefix;
         result += '<span class="keyword">' + instance.text.substring(instance.start, instance.end) + '</span>';
         if (suffix) result += suffix;
-        if (instance.end + context + 1 < instance.text.length) result += '...';
-        results.push(result);
+        return result;
     }
-    return results.join(' ');
-}
 
-function getTitle(matches, unmatchedTitle) {
-    let instances = getInstances(matches, true);
-    if (instances.length == 0)
-        return unmatchedTitle;
-    // Only consider the first 
-    let instance = instances[0];
-    let prefix = instance.text.substring(0, instance.start);
-    let suffix = instance.text.substring(instance.end);
-    let result = '';
-    if (prefix) result += prefix;
-    result += '<span class="keyword">' + instance.text.substring(instance.start, instance.end) + '</span>';
-    if (suffix) result += suffix;
-    return result;
-}
-
-function getResultsHtml(results) {
-    if (results == null) {
-        return '';
-    }
-    if (results.length == 0) {
-        return '<div class="dropdown-item empty">Nic nenalezeno</div>';
-    }
-    var resultsHtml = '';
-    results.forEach(function (res) {
-        let item = res.item;
-        snippet = getSnippet(res.matches);
-        title = getTitle(res.matches, item.title);
-        resultsHtml += '<a class="dropdown-item clearfix" href="' + item.url + '">';
-        resultsHtml += '<div class="title">' + title + '</div>';
-        resultsHtml += '<div class="search-preview card">' + item.block + '</div>' +
-                       '<div class="snippet">' + snippet + '</div>' +
-                       '</a>';
-    });
-    return resultsHtml;
-}
-
-function updateResults(results) {
-    $('#omnisearch-suggestions').html(getResultsHtml(results));
-    $('#omnisearch-suggestions a:first-of-type').addClass('active');
-}
-
-function postFilter(data) {
-    // TODO: strip {% ... %}
-    return data;
-}
-
-function fetchSiteJson(callback) {
-    if (posts.length === 0) {
-        // fetch site.json file
-        jQuery.get("/search.json", function (data) {
-            posts = postFilter(data);
-            var options = { 		// initialize options for fuse.js
-                shouldSort: true,
-                threshold: 0.3,
-                ignoreLocation: true,
-                maxPatternLength: 32,
-                minMatchCharLength: 2,
-                includeMatches: true,
-                includeScore: true,
-                keys: [
-                    {
-                        name: "title",
-                        weight: 0.2		// give title more importance
-                    },
-                    {
-                        name: "perex",
-                        weight: 0.3		// give perex more importance
-                    },
-                    {
-                        name: "content",
-                        weight: 0.6
-                    }
-                ]
-            };
-            fuse = new Fuse(posts, options);
-            callback();
-        }).fail(function() {
-            // TODO: handle failure gracefully.
-        });
-    } else { // we already have the posts so simply use it instead of downloading the file again
-        callback();
+    _getKeywordInstances(matches, shouldMatchTitle) {
+        let instances = [];
+        if (!matches)
+            return instances;
+        for (const match of matches) {
+            let isTitle = (match.key == 'title');
+            if (isTitle != shouldMatchTitle) {
+                continue;
+            }
+            for (const index of match.indices) {
+                // Ignore matches of length 1;
+                if (index[1] - index[0] == 0) continue;
+                instances.push({start: index[0], end: index[1] + 1, text: match.value});
+            }
+        }
+        // Sort by match length, descending.
+        instances.sort((a,b) => (b.end - b.start) - (a.end - a.start));
+        return instances;
     }
 }
