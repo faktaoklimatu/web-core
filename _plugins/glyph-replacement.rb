c8c60681b187c2242e0e314c3015d58a605d5b45
Martin Ukrop <mukrop@mail.muni.cz>
2021-04-23 10:38:53 +0200

    Fix glyph replacement plugin
    The replace function was moved within the definition of the hook to not clash with the same function defined at the local language version.

--- _plugins/glyph-replacement.rb
+++ _plugins/glyph-replacement.rb
@@ -7,12 +7,13 @@ Jekyll::Hooks.register :site, :post_render do |site|
     Jekyll.logger.error "Undefined page.output for '#{page.path}'. Consider excluding this file" if not page.respond_to? :output
     replace!(page.output)
   end
-end
 
-def replace!(content)
-  # Thin spaces before percent sign, permille sign and before groups of digits.
-  content.gsub!(/(?<=\d) (%|‰|\d{3})/, '&#8239;\1')
-  # Non-breaking spaces before units.
-  content.gsub!(/(?<=\d) (°C|ppm|kg|mil\.)/, '&nbsp;\1')
-  content.gsub!(/(?<=\d) ([kMGT]?(?:t CO|Wh?))/, '&nbsp;\1')
+  def replace!(content)
+    # Thin spaces before percent sign, permille sign and before groups of digits.
+    content.gsub!(/(?<=\d) (%|‰|\d{3})/, '&#8239;\1')
+    # Non-breaking spaces before units.
+    content.gsub!(/(?<=\d) (°C|ppm|kg|mil\.)/, '&nbsp;\1')
+    content.gsub!(/(?<=\d) ([kMGT]?(?:t CO|Wh?))/, '&nbsp;\1')
+  end
+  
 end
