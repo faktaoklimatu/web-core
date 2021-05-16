a9d2a674eec260e7d990c21d63d9c5fb5113d7bc
Martin Ukrop <mukrop@mail.muni.cz>
2021-05-16 20:57:23 +0200

    Adjust automatic space substitution
    Use regular non-breaking space before currencies and percent/promile instead of nonbreaking thin space.
    (This is a result of discussion with @Jirka-tisnov.)

--- _plugins/space-replacement.rb
+++ _plugins/space-replacement.rb
@@ -4,10 +4,10 @@ Jekyll::Hooks.register :site, :post_render do |site|
   Jekyll.logger.info  "                  * Replacing special spaces ..."
 
   def replace!(content)
-    # Thin spaces before percent sign, permille sign and before groups of digits.
-    content.gsub!(/(?<=\d) (%|‰|€|$|\d{3})/, '&#8239;\1')
+    # Thin spaces before groups of digits.
+    content.gsub!(/(?<=\d) (\d{3})/, '&#8239;\1')
     # Non-breaking spaces before units.
-    content.gsub!(/(?<=\d) (°C|ppm|kg|mil\.)/, '&nbsp;\1')
+    content.gsub!(/(?<=\d) (%|‰|€|$|°C|ppm|kg|mil\.)/, '&nbsp;\1')
     content.gsub!(/(?<=\d) ([kMGT]?(?:t CO|Wh?))/, '&nbsp;\1')
   end
 
