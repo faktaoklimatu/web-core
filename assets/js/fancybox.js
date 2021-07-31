4cc5d42bf368be34b11b915609d45ca382746b49
Martin Zak <ufonzak@gmail.com>
2021-07-31 11:13:31 -0700

    Adding fancybox v3

---
+++ assets/js/fancybox.js
@@ -0,0 +1,29 @@
+(function() {
+  const i18n = {
+    cs: {
+      CLOSE: 'Zavřít',
+      NEXT: 'Další',
+      PREV: 'Předchozí',
+      DOWNLOAD: 'Stáhnout',
+    },
+    sk: {
+      CLOSE: 'Zavrieť',
+      NEXT: 'Ďalší',
+      PREV: 'Predchádzajúca',
+      DOWNLOAD: 'Stiahnuť',
+    },
+  };
+
+  const defaults = $.fancybox.defaults;
+  defaults.lang = window.fakta.lang;
+  defaults.buttons = [
+    'download',
+    'close',
+  ];
+  for (const [lang, values] of Object.entries(i18n)) {
+    defaults.i18n[lang] = {
+      ...defaults.i18n.en,
+      ...values,
+    };
+  }
+})();
