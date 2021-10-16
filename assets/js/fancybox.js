d18fa1375da91d898fdcfbc9a4c879018dafb597
Martin Zak <ufonzak@gmail.com>
2021-10-16 10:53:20 -0700

    Adding custom caption to fancybox

--- assets/js/fancybox.js
+++ assets/js/fancybox.js
@@ -26,4 +26,12 @@
       ...values,
     };
   }
+  defaults.beforeLoad = function(instance, current) {
+    const customCaption = current.opts.$orig[0].parentElement.querySelector('.fancybox-custom-caption');
+    if (customCaption) {
+      const captionBody = instance.$refs.caption[0].querySelector('.fancybox-caption__body');
+      captionBody.innerHTML = '';
+      captionBody.appendChild(customCaption.cloneNode(true));
+    }
+  };
 })();
