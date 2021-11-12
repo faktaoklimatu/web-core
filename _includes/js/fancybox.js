0da21bff4ab0faa59bbb04c4f0e3776e7da4081e
Martin Zak <ufonzak@gmail.com>
2021-11-11 18:07:31 -0800

    Using translation file.

--- _includes/js/fancybox.js
+++ _includes/js/fancybox.js
@@ -1,31 +1,14 @@
 (function() {
-  const i18n = {
-    cs: {
-      CLOSE: 'Zavřít',
-      NEXT: 'Další',
-      PREV: 'Předchozí',
-      DOWNLOAD: 'Stáhnout',
-    },
-    sk: {
-      CLOSE: 'Zavrieť',
-      NEXT: 'Ďalší',
-      PREV: 'Predchádzajúca',
-      DOWNLOAD: 'Stiahnuť',
-    },
-  };
-
   const defaults = $.fancybox.defaults;
-  defaults.lang = window.fakta.lang;
+
+  defaults.lang = {{ site.lang | jsonify }};
+  defaults.i18n[defaults.lang] = {{ site.data.lang.fancybox | jsonify }};
+
   defaults.buttons = [
     'zoom',
     'close',
   ];
-  for (const [lang, values] of Object.entries(i18n)) {
-    defaults.i18n[lang] = {
-      ...defaults.i18n.en,
-      ...values,
-    };
-  }
+
   defaults.beforeLoad = function(instance, current) {
     const customCaption = current.opts.$orig[0].parentElement.querySelector('.fancybox-custom-caption');
     if (customCaption) {
