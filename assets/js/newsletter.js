ecd72b357f539186d6442014252fe12413a546ab
Martin Ukrop <mukrop@mail.muni.cz>
2022-03-04 16:14:17 +0100

    Add JS to open newsletter modal based on URL

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,17 +1,17 @@
 $(document).ready(() => {
     $('#newsletter-modal').one('show.bs.modal', (event) => {
         const newsletterId = event.target.dataset.newsletterId;
-
         window['ecm-widget'] = 'ecmwidget';
         window['ecmwidget'] = window['ecmwidget'] || function() {
             (window['ecmwidget'].q = window['ecmwidget'].q || []).push(arguments)
         };
-
         const js = document.createElement('script');
         js.id = newsletterId;
         js.dataset.a = 'faktaoklimatu';
         js.src = 'https://d70shl7vidtft.cloudfront.net/widget.js';
-
         document.head.appendChild(js);
     });
+    if(window.location.href.indexOf('#newsletter') != -1) {
+        $('#newsletter-modal').modal('show');
+    }
 });
