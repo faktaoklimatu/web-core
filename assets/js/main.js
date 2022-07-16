caae45a26f9b14e554b8508e76ba0c1d2973b736
Jan Krčál <jan.krcal@faktaoklimatu.cz>
2022-07-16 15:41:07 +0200

    Minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     if ($("#dev-warning").length == 1) {
         console.log('This is the local development version of the website.');
     }
