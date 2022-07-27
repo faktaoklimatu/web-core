daa32874671172556f53c863ec6d5a43bef0c6a6
Jan Krčál <jan.krcal@faktaoklimatu.cz>
2022-07-27 12:22:28 +0200

    Fix a js bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,7 @@ $(document).ready(function() {
 
     // Custom expanders for expandable preview blocks.
     $(".preview-blocks-expander .expander").click(function() {
+        let buttonCollapsedClass = "collapsed";
         // Animate the chevron icon.
         $(this).toggleClass(buttonCollapsedClass);
         // Swap the "More" / "Less" labels.
@@ -45,7 +46,6 @@ $(document).ready(function() {
 
         // Show / hide the preview blocks. Do a 2-phase transition to make sure the 'display' property does not change
         // in one of the phases. This is the animated phase (change in 'display' blocks css transition to happen).
-        let buttonCollapsedClass = "collapsed";
         let blockExpandingClass = "expanding";
         let blockExpandedClass = "expanded";
         if (expandables.hasClass(blockExpandedClass)) {
