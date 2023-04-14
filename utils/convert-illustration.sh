fe78ce0a5aa10bc4ef30ecff7ebbf31f3410091d
Jan Krčál <jan.krcal@faktaoklimatu.cz>
2023-04-14 17:23:44 +0200

    Factor out desired width into a constant

--- utils/convert-illustration.sh
+++ utils/convert-illustration.sh
@@ -13,6 +13,8 @@ source utils/convert-svg.sh
 # Exit when any command fails
 set -e
 
+WIDTH=1200
+
 # Set up file names
 SRC_FILE_SVG=$1
 DST_FILE_SVG=$2
@@ -20,9 +22,9 @@ DST_FILE_SVG=$2
 # Create destination folders if necessary
 mkdir -p $(dirname $DST_FILE_SVG)
 
-# Convert SVG into PNG of width 1200px.
-echo -e `basename $SRC_FILE_SVG`": converting to PNG (${width}px)..."
-convert_svg_to_png "$SRC_FILE_SVG" "$DST_FILE_SVG" 1200
+# Convert SVG into PNG of desired $WIDTH.
+echo -e `basename $SRC_FILE_SVG`": converting to PNG ($WIDTH px)..."
+convert_svg_to_png "$SRC_FILE_SVG" "$DST_FILE_SVG" $WIDTH
 
 # If all previous conversions pass, copy the file checked by Make
 echo `basename $SRC_FILE_SVG`": copying to $DST_FILE_SVG..."
